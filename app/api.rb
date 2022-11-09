e5474bfbb68db307c2714c1d989cd8d4495ef366
starkiller17 <eduardo.said.mndz@gmail.com>
2022-11-08 22:05:03 -0600

    refactor post_expense add more expenses and start get expenses by date

--- app/api.rb
+++ app/api.rb
@@ -3,5 +3,12 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    post '/expenses' do
+      JSON.generate( "expense_id": 42 )
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
   end
 end
--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -11,13 +11,40 @@ module ExpenseTracker
     end
 
     it 'records submitted expenses' do
-      coffee = {
+      pending 'Need to persist expenses'
+      coffee = post_expense(
         'payee' => 'Starbucks',
         'amount' => 5.75,
         'date' => '2017-06-10'
-      }
+      )
 
-      post '/expenses', JSON.generate(coffee)
+      zoo = post_expense(
+        'payee' => 'Zoo',
+        'amount' => 15.25,
+        'date' => '2017-06-10'
+      )
+
+      groceries = post_expense(
+        'payee' => 'Whole Foods',
+        'amount' => 95.20,
+        'date' => '2017-06-11'
+      )
+
+      get '/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+      expenses = JSON.parse(last_response.body)
+      # Expecting an array that contains coffee and zoo expenses without regard to the order
+      expect(expenses).to contain_exactly(coffee, zoo)
+    end
+
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      # A hash containing a key of 'expense_id' and an integer value
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
     end
   end
 end
